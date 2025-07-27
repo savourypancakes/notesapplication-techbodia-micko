3c3d7c1965bbc8110f03922b559a109be22555bf
Micko Kok <kokmicko@gmail.com>
2025-07-27 11:18:52 +0700

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
+    base: '/notesapplication-techbodia-micko/',
     plugins: [
       tailwindcss(),
     ],
