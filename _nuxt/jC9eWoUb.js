3a97040f0bad662f5eb40b93f3e0cccceeb3b3e2
Micko Kok <kokmicko@gmail.com>
2025-07-27 14:21:05 +0700

    Updates

---
+++ _nuxt/jC9eWoUb.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,o as r,s as t}from"./23r9y9ul.js";const n={};function c(e,a){return r(),s("tbody",null,[t(e.$slots,"default")])}const l=Object.assign(o(n,[["render",c]]),{__name:"ProseTbody"});export{l as default};
---
+++ frontend/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/kokmi/Documents/GitHub/notesapplication-techbodia-micko/frontend/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
