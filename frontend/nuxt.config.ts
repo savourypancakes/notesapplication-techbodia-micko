532ecb0af2593ffe2fb89c0f9c3263cc6ab93125
Micko Kok <kokmicko@gmail.com>
2025-07-28 10:51:18 +0700

    changes

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
