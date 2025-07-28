6245bc24c6b519e7fa623064a08202c559efd663
Micko Kok <kokmicko@gmail.com>
2025-07-28 10:34:41 +0700

    changes

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   app: {
-    baseURL: '/',
+    baseURL: '/notesapplication-techbodia-micko/',
   },
   nitro: {
     preset: 'static',
